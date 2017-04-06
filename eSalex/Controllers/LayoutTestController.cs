1581004ab7b47ce4dc3acadf890f59e085136a85
yushan0412 <User@F160520>
2017-04-06 22:50:20 +0800

    3-4

---
+++ eSalex/Controllers/LayoutTestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eSalex.Controllers
+{
+    public class LayoutTestController : Controller
+    {
+        // GET: LayoutTest
+
+        /// <summary>
+        /// 測試主版頁面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
